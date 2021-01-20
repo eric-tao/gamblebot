53be857a7c4175ccd6f37de585f407a1e44a98f0
wilsonxu94 <wilson.xu.94@gmail.com>
2021-01-19 21:13:44 -0500

    adding crypto function from coincap

--- bot.py
+++ bot.py
@@ -33,6 +33,15 @@ class DBHelper:
         else:
             print("Refusing to insert due to wrong number of args")
 
+    def add_crypto_index(self,table,values=()):
+        stmt = 'INSERT INTO %s (name) VALUES(?)'%(table)
+        args = values
+        if len(args) == 1:
+            self.conn.execute(stmt,args)
+            self.conn.commit()
+        else:
+            print("Refusing to insert due to wrong number of args")
+
     def get_item_by_time(self,table,time):
         stmt = 'SELECT * FROM %s WHERE date > ? ORDER BY date DESC LIMIT 1'%(table)
         args = (time.isoformat(sep=' '),)
@@ -44,28 +53,29 @@ class MyClient(discord.Client):
     async def on_ready(self):
         self.db = DBHelper()
         self.db.setup()
-        self.coindesk_api = "https://api.coindesk.com/v1/bpi/currentprice.json"
+        self.coincap_api = "api.coincap.io/v2/asset/"
         self.time_delay = 5
         self.commands_msg = "Subcommands supported:\n```btc: display price of BTC in the past 15 min\nroll: with a xdy argument, roll x dice\n```"
         print('Logged on as', self.user)
 
-    def get_cached_btc_price(self):
+    def get_asset_price(self, asset):
         now = datetime.datetime.now()
-        cache_time = now - datetime.timedelta(minutes=self.time_delay)
-        item = self.db.get_item_by_time("crypto_prices",cache_time)
-        if len(item) == 0:
-            request = requests.get(self.coindesk_api)
-            while request.status_code != 200:
-                sleep(10)
-                request = requests.get(self.coindesk_api)
-            time = datetime.datetime.fromisoformat(request.json()['time']['updatedISO'])
-            usd_rate = request.json()['bpi']['USD']['rate_float']
-            self.db.add_item("crypto_prices",(time,'BTC',usd_rate))
-            return time,usd_rate
-        else:
-            time,name,price = item[0]
-            time = datetime.datetime.fromisoformat(time)
-            return time,price
+        requestString = self.coincap_api + asset
+        request = requests.get(requestString)
+        while request.status_code != 200:
+            sleep(10)
+            request = requests.get(requestString)
+        time = now
+        usd_rate = request.json()['data']['priceUsd']
+        return usd_rate
+
+    @bot.command(name="crypto")
+    async def _crypto(self, arg1):
+        price = get_asset_price(arg1)
+        embed = discord.Embed()
+        embed.description = "Powered by CoinCap"
+        content = "The price of %s is %s USD"%(arg1,price)
+        await message.channel.send(content=content,embed=embed)
 
     async def on_message(self,message):
         if message.author == self.user:
@@ -73,12 +83,12 @@ class MyClient(discord.Client):
         elif re.match(r'^gamble\b', message.content) is not None:
             if len(message.content.split()) == 1:
                 await message.channel.send(self.commands_msg)
-            elif message.content.split()[1] == 'btc':
-                time,price =  self.get_cached_btc_price()
-                embed = discord.Embed()
-                embed.description = "Powered by [CoinDesk](https://www.coindesk.com/price/bitcoin)"
-                content = "The price of BTC at %s was %s USD\nThis bot will only query the price if cached data is older than %s minutes to avoid overuse of the API."%(time.ctime(),price,str(self.time_delay))
-                await message.channel.send(content=content,embed=embed)
+            # elif message.content.split()[1] == 'btc':
+            #     time,price =  self.get_cached_btc_price()
+            #     embed = discord.Embed()
+            #     embed.description = "Powered by [CoinDesk](https://www.coindesk.com/price/bitcoin)"
+            #     content = "The price of BTC at %s was %s USD\nThis bot will only query the price if cached data is older than %s minutes to avoid overuse of the API."%(time.ctime(),price,str(self.time_delay))
+            #     await message.channel.send(content=content,embed=embed)
             elif message.content.split()[1] == 'roll':
                 if len(message.content.split()) == 2:
                      await message.channel.send("Roll usage:\n```gamble roll xdy: rolls x dice with a range from 1 to y```")
